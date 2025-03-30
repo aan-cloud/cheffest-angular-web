87f161cdad163709385a3d159b4463b6a829c633
aan-cloud <evirsanti@gmail.com>
2025-03-30 18:21:30 +0800

    feat: layout component

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -5,6 +5,7 @@ import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
+  standalone: true,
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
