4482c24d6de44a8fe98feba62d3688c21b0f03ba
aan-cloud <evirsanti@gmail.com>
2025-03-30 18:21:51 +0800

    feat: navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,47 +1,86 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
+import { signal } from '@angular/core';
+import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 import { GoogleLoginProvider, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { ToastrService } from 'ngx-toastr';
+
+type LoginResponse = {
+  message: string,
+  token: string
+};
 
 @Component({
   selector: 'app-navbar',
-  imports: [CommonModule, RouterLink],
+  standalone: true,
+  providers: [CookieService],
+  imports: [CommonModule, RouterLink, GoogleSigninButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-  isAuth = false;
-  showPopover = false;
-  userdata: SocialUser | null = null;
+  isAuth = signal(false);
+  showPopover = signal(false);
+  userdata = signal<SocialUser | null>(null);
+  private cookieService = inject(CookieService);
 
-  constructor(private router: Router, private authService: SocialAuthService) {}
-
-  isActive(path: string): { [klass: string]: boolean } {
-    return { 'active-class': window.location.pathname === path };
-  }
+  constructor(private toasterService: ToastrService, private router: Router, private authService: SocialAuthService, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.authService.authState.subscribe((user) => {
-      this.userdata = user;
-      this.isAuth = !!user; // True jika user ada, false jika tidak
+      console.log(user.photoUrl)
+      
+      if (user) {
+        const url = "https://cheffest-backend-spring-production.up.railway.app/api/auth/google";
+        const payload = { token: user.idToken };
+        const headers = new HttpHeaders({
+          'Content-Type': 'application/json'
+        });
+
+        this.http.post<LoginResponse>(url, payload, { headers }).subscribe({
+          next: (data) => {
+            const expiryDate = new Date();
+            expiryDate.setMinutes(expiryDate.getMinutes() + 15);
+    
+            this.cookieService.set('token', data.token, {expires: expiryDate, path: '/'});
+            this.userdata.update(() => (user));
+            this.isAuth.set(!!user);
+            this.toasterService.success('Login Success!', 'Success', {timeOut: 3000})
+          },
+          error: (error) => {
+            console.log(`fetch login error: ${error}`)
+          }
+        });
+      }
     });
   }
+
 
   handleSearch(event: KeyboardEvent): void {
     console.log("Search triggered", (event.target as HTMLInputElement).value);
   }
 
   togglePopover(): void {
-    this.showPopover = !this.showPopover;
+    this.showPopover.set(!this.showPopover());
+    console.log(this.showPopover())
   }
 
   handleLogout(): void {
-    this.isAuth = false;
-    this.userdata = null;
+    this.authService.signOut().then(() => {
+      this.cookieService.delete('token');
+      this.isAuth.set(false);
+      this.userdata.set(null);
+      this.toasterService.success('Logout Success!', 'Success', {timeOut: 2000})
+    });
   }
 
   handleLogin(): void {
-    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).catch(error => {
+      console.error("Login error:", error);
+    });
   }
 
   onLoginError(): void {
