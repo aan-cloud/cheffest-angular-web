637c30b7d0b5463aa7deac23d92c9f65db5d6ad5
aan-cloud <evirsanti@gmail.com>
2025-03-30 17:04:59 +0800

    feat: routes and app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideToastr(),
+    provideAnimations(),
+    importProvidersFrom(SocialLoginModule),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('15061094991-19alsf2g3hp0sp81msq2up21jja1060i.apps.googleusercontent.com'),
+          },
+        ],
+      } as SocialAuthServiceConfig,
+    },
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FoodsComponent } from './pages/foods/foods.component'
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
     {
@@ -9,8 +10,8 @@ export const routes: Routes = [
         component: LayoutComponent,
         children: [
           { path: '', component: HomeComponent },
-          { path: 'foods', component: FoodsComponent}
-        //   { path: 'about', component: AboutComponent }
+          { path: 'foods', component: FoodsComponent},
+          { path: 'about', component: AboutComponent }
         ]
     }
 ];
